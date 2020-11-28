ad7a7c56a12230c0493e801023269389e5b9d5c5
tomasv10 <tomasvarnauskas19@gmail.com>
2020-11-28 17:01:42 +0200

    changed backend base url

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { error } from "shelljs";
 Vue.use(Vuex);
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+axios.defaults.baseURL = "http://localhost/todo-laravel/api";
 
 export const store = new Vuex.Store({
   state: {
